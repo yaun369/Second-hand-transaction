09be4a6bcfea2f6196dd35454b43f787384092cc
geekreading <geekreading@163.com>
2019-04-02 22:12:14 +0800

    cms小程序；

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners: ["http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg", "http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/53f44ecb4041b0ed8085347bb77f858a.jpg", "http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/f25d00e340db95f580430ee6757f5bd5.jpg"],
+    navigation: [{
+      src: '../../images/read.png',
+      title: '图书'
+    }, {
+      src: '../../images/sport.png',
+      title: '体育'
+    }, {
+      src: '../../images/camera.png',
+      title: '数码'
+    }, {
+      src: '../../images/image.png',
+      title: '作品'
+    }, {
+      src: '../../images/like.png',
+      title: '喜欢'
+    }, {
+      src: '../../images/user.png',
+      title: '联系'
+    }, {
+      src: '../../images/ups.png',
+      title: '秀'
+    }, {
+      src: '../../images/star.png',
+      title: '收藏'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
