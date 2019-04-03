c6d2f8202bd89bd514c383e80edc4cd2c497d820
geekreading <geekreading@163.com>
2019-04-04 00:41:18 +0800

    submit,class;

--- pages/class/class.js
+++ pages/class/class.js
@@ -5,62 +5,122 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    classArray: [{
+      title: '平台推荐',
+      color: '#1296db',
+      classItem: [{
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }]
+    }, {
+      title: '最近更新',
+      color: '#000',
+      classItem: [{
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }]
+    }, {
+      title: '近期热门',
+        color: '#0fd8a4',
+      classItem: [{
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }, {
+        cover: 'http://bmob-cdn-20041.b0.upaiyun.com/2018/06/28/a7da358740a1268a80bd00998be8b7b6.jpg',
+        subtitle: '图书',
+        viewNum: 1289
+      }]
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-    preview:['../../images/add.png']
+    preview: ['../../images/add.png']
   },
-
+  formSubmit(e) {
+    console.log(e.detail.value);
+  },
+  updataImages() {
+    const that = this;
+    wx.chooseImage({
+      count: 6,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        const tempFilePaths = res.tempFilePaths;
+        that.setData({
+          preview: tempFilePaths
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
